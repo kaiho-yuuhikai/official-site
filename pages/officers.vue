44116839c073caeb0ddf16e4b7eeb936aea7643d
gsc-kamiya <gsc.kamiya@h2o-retailing.co.jp>
2026-04-06 05:35:11 +0900

    feat: みねけんのプロフィール写真を追加・Threads自動更新をpush時にも実行

--- pages/officers.vue
+++ pages/officers.vue
@@ -135,7 +135,7 @@ const members = [
   { name: 'じょーじ', generation: '13期・14期 理数科', initial: 'じ', bgClass: getBgClass(0), note: 'GScale 代表取締役 | Google Developer Expert', photo: 'images/members/george.png' },
   { name: 'うえま', generation: '16期 理数科', initial: 'う', bgClass: getBgClass(1), note: '広報コーディネーター', photo: 'images/members/uema.png' },
   { name: 'かえ', generation: '20期 英語科', initial: 'か', bgClass: getBgClass(2), note: '経営・バレエ講師', photo: 'images/members/kae.jpg' },
-  { name: 'みねけん', generation: '18期 理数科', initial: 'み', bgClass: getBgClass(3), note: '元カメラマン・探究活動事務局', photo: '' },
+  { name: 'みねけん', generation: '18期 理数科', initial: 'み', bgClass: getBgClass(3), note: '元カメラマン・探究活動事務局', photo: 'images/members/mineken.png' },
 ]
 
 const teams = [
